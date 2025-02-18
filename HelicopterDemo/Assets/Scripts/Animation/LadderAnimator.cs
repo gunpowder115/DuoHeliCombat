fc6fd6f3334d95b30cb0a0e8382abf8d0466caeb
gunpowder115 <vk.poroh.alex@bk.ru>
2025-02-18 16:03:34 +0300

    Added ladder exit animation

--- HelicopterDemo/Assets/Scripts/Animation/LadderAnimator.cs
+++ HelicopterDemo/Assets/Scripts/Animation/LadderAnimator.cs
@@ -2,18 +2,71 @@ using UnityEngine;
 
 public class LadderAnimator : MonoBehaviour
 {
+    [SerializeField] private bool ladderExit = false;
     [SerializeField] private int ladderCount = 6;
     [SerializeField] private float partOffset = 0.6f;
+    [SerializeField] private float exitSpeed = 1f;
     [SerializeField] private GameObject ladderPartPrefab;
     [SerializeField] private GameObject[] menPrefabs;
 
     private GameObject[] ladderParts;
     private GameObject[] men;
+    private Renderer[] rends;
+    private Vector3 speed;
+    private Vector3 targetPos, currentPos;
+    private bool menInitialized;
+    private int currentPart;
 
     void Start()
     {
+        InitLadder();
+        if (!ladderExit)
+        {
+            InitMenOnLadder();
+            menInitialized = true;
+        }
+
+        rends = GetComponentsInChildren<Renderer>();
+        if (ladderExit)
+        {
+            for (int i = 0; i < rends.Length - 1; i++)
+                rends[i].enabled = false;
+            targetPos = transform.position;
+            transform.Translate(new Vector3(0f, partOffset * ladderCount, 0f));
+            currentPos = transform.position;
+            currentPart = rends.Length - 2;
+        }
+        speed = new Vector3(0f, exitSpeed, 0f);
+    }
+
+    private void Update()
+    {
+        if (ladderExit)
+        {
+            transform.Translate(-speed * Time.deltaTime);
+
+            if (currentPos.y - transform.position.y > partOffset && currentPart >= 0)
+            {
+                rends[currentPart--].enabled = true;
+                currentPos = transform.position;
+            }
+
+            if (transform.position.y <= targetPos.y)
+            {
+                transform.position = targetPos;
+                ladderExit = false;
+                if (!menInitialized)
+                {
+                    InitMenOnLadder();
+                    menInitialized = true;
+                }
+            }
+        }
+    }
+
+    private void InitLadder()
+    {
         ladderParts = new GameObject[ladderCount];
-        men = new GameObject[3];
 
         for (int i = 0; i < ladderCount; i++)
         {
@@ -26,17 +79,20 @@ public class LadderAnimator : MonoBehaviour
                 ladderParts[i].transform.position = new Vector3(pos.x, pos.y - partOffset, pos.z);
             }
         }
+    }
+
+    private void InitMenOnLadder()
+    {
+        men = new GameObject[3];
 
         if (ladderParts.Length >= 3 && menPrefabs.Length >= 1) men[0] = Instantiate(menPrefabs[0], ladderParts[0].transform);
         if (ladderParts.Length >= 6 && menPrefabs.Length >= 1) men[1] = Instantiate(menPrefabs[1], ladderParts[3].transform);
         if (ladderParts.Length >= 9 && menPrefabs.Length >= 1) men[2] = Instantiate(menPrefabs[2], ladderParts[6].transform);
 
-
-
-        for(int i = 0; i < men.Length; i++)
+        for (int i = 0; i < men.Length; i++)
         {
             float xScale = Random.Range(0, 2) == 0 ? 1f : -1f;
-            men[i].transform.localScale = new Vector3(xScale, 1f, i % 2 == 0 ? 1f : -1f);
+            if (men[i]) men[i].transform.localScale = new Vector3(xScale, 1f, i % 2 == 0 ? 1f : -1f);
         }
     }
 }
