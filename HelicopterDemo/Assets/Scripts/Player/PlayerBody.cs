66a2e80107c3680f6b06d17a6ef0f4d2df86f091
gunpowder115 <vk.poroh.alex@bk.ru>
2025-03-17 18:04:54 +0300

    Added key grabbing by player; key setting to flag vault; vault opening

--- HelicopterDemo/Assets/Scripts/Player/PlayerBody.cs
+++ HelicopterDemo/Assets/Scripts/Player/PlayerBody.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerBody : MonoBehaviour
 {
     [SerializeField] private GameObject bombHolder;
+    [SerializeField] private GameObject flagHolder;
+    [SerializeField] private GameObject keyHolder;
 
     public PickableUp ItemForTake { get; set; }
     public PickableUp Item { get; private set; }
@@ -17,14 +19,32 @@ public class PlayerBody : MonoBehaviour
     {
         if (Item)
         {
-            Item.transform.position = bombHolder.transform.position;
-            Item.transform.rotation = bombHolder.transform.rotation;
+            Bomb bomb = ItemForTake.GetComponent<Bomb>();
+            Flag flag = ItemForTake.GetComponent<Flag>();
+            Key key = ItemForTake.GetComponent<Key>();
+            if (bomb)
+            {
+                Item.transform.position = bombHolder.transform.position;
+                Item.transform.rotation = bombHolder.transform.rotation;
+            }
+            else if (flag)
+            {
+                Item.transform.position = flagHolder.transform.position;
+                Item.transform.rotation = flagHolder.transform.rotation;
+            }
+            else if (key)
+            {
+                Item.transform.position = keyHolder.transform.position;
+                Item.transform.rotation = keyHolder.transform.rotation;
+            }
         }
     }
 
     public void Take()
     {
         Bomb bomb = ItemForTake.GetComponent<Bomb>();
+        Flag flag = ItemForTake.GetComponent<Flag>();
+        Key key = ItemForTake.GetComponent<Key>();
         if (bomb && bombHolder)
         {
             Item = ItemForTake;
@@ -35,15 +55,57 @@ public class PlayerBody : MonoBehaviour
             Item.SetTrigger(false);
             bomb.IsActivated = true;
         }
+        else if (flag && flagHolder)
+        {
+            Item = ItemForTake;
+            Item.transform.SetParent(transform);
+            Item.transform.position = flagHolder.transform.position;
+            Item.transform.rotation = flagHolder.transform.rotation;
+            Item.transform.localScale = new Vector3(Item.transform.localScale.x, -Item.transform.localScale.y, Item.transform.localScale.z);
+            Item.SetGravity(false);
+            Item.SetTrigger(false);
+        }
+        else if (key && keyHolder)
+        {
+            Item = ItemForTake;
+            Item.transform.SetParent(transform);
+            Item.transform.position = keyHolder.transform.position;
+            Item.transform.rotation = keyHolder.transform.rotation;
+            Item.SetGravity(false);
+            Item.SetTrigger(false);
+        }
     }
 
     public void Drop()
     {
         if (Item)
         {
-            Item.transform.SetParent(null);
-            Item.SetGravity(true);
-            Item = null;
+            if (Item.GetComponent<Key>())
+            {
+                //todo remove tags
+                GameObject vault = GameObject.FindGameObjectWithTag("EnemyFlagVault");
+                if (vault)
+                {
+                    float dist = Vector3.Magnitude(vault.transform.position - transform.position);
+                    if (dist < 10)
+                    {
+                        vault.GetComponent<FlagVault>().SetKey(Types.KeyType.Purple);
+                        Destroy(Item.gameObject);
+                    }
+                    else
+                    {
+                        Item.SetGravity(true);
+                        Item.transform.SetParent(null);
+                        Item = null;
+                    }
+                }
+            }
+            else
+            {
+                Item.transform.SetParent(null);
+                Item.SetGravity(true);
+                Item = null;
+            }
         }
     }
 }
