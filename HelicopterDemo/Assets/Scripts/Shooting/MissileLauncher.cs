b010daf1134bc96e5341e23a1685fda3e5b62241
gunpowder115 <vk.poroh.alex@bk.ru>
2025-04-29 21:12:12 +0300

    Refactoring

--- HelicopterDemo/Assets/Scripts/Shooting/MissileLauncher.cs
+++ HelicopterDemo/Assets/Scripts/Shooting/MissileLauncher.cs
@@ -26,35 +26,39 @@ public class MissileLauncher : BaseLauncher
 
     public void Launch(GameObject target)
     {
-        this.target = target;
-        if (missilePrefab)
+        if (currVolume >= maxClipVolume)
         {
-            if (guided)
+            this.target = target;
+            if (missilePrefab)
             {
-                var missileItem = Instantiate(missilePrefab, transform.position + transform.forward * 2f, transform.rotation);
-                GuidedMissile guidedMissile = missileItem.GetComponent<GuidedMissile>();
-                guidedMissile.Side = Side;
-                guidedMissile.IsPlayer = IsPlayer;
-                if (guidedMissile) guidedMissile.SelectedTarget = target;
+                if (guided)
+                {
+                    var missileItem = Instantiate(missilePrefab, transform.position + transform.forward * 2f, transform.rotation);
+                    GuidedMissile guidedMissile = missileItem.GetComponent<GuidedMissile>();
+                    guidedMissile.Side = Side;
+                    guidedMissile.IsPlayer = IsPlayer;
+                    if (guidedMissile) guidedMissile.SelectedTarget = target;
+                }
+                else
+                {
+                    var missile = Instantiate(missilePrefab, transform.position + transform.forward * 2f, CalculateDeflection()).GetComponent<Projectile>();
+                    missile.Side = Side;
+                    missile.IsPlayer = IsPlayer;
+                }
             }
             else
-            {
-                var missile = Instantiate(missilePrefab, transform.position + transform.forward * 2f, CalculateDeflection()).GetComponent<Projectile>();
-                missile.Side = Side;
-                missile.IsPlayer = IsPlayer;
-            }
+                Debug.Log(this.ToString() + ": missilePrefab is NULL!");
+            SetMissileActive(false);
+            uiCircle?.SetEmptyColor();
+            isRecharge = true;
+            currVolume -= 1f;
+            currShotDeltaTime = 0f;
         }
-        else
-            Debug.Log(this.ToString() + ": missilePrefab is NULL!");
-        SetMissileActive(false);
-        uiCircle?.SetEmptyColor();
-        isRecharge = true;
-        currVolume = 0f;
-        currShotDeltaTime = 0f;
     }
 
     void Start()
     {
+        currVolume = maxClipVolume;
         isEnable = true;
         childObjects = new GameObject[transform.childCount];
         for (int i = 0; i < transform.childCount; i++)
@@ -69,6 +73,7 @@ public class MissileLauncher : BaseLauncher
         {
             currVolume = maxClipVolume;
             uiCircle?.SetFillColor();
+            SetMissileActive(true);
             isRecharge = false;
         }
         uiCircle?.SetCircleAmount(NormClipVolume);
