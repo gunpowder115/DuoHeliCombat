2f5ae7137b33072c3236bbeb8d97e33a8cb6dca5
gunpowder115 <vk.poroh.alex@bk.ru>
2025-06-18 21:10:30 +0300

    Checking existing of second camera

--- HelicopterDemo/Assets/Scripts/Controllers/CamerasController.cs
+++ HelicopterDemo/Assets/Scripts/Controllers/CamerasController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CamerasController : MonoBehaviour
@@ -7,7 +8,7 @@ public class CamerasController : MonoBehaviour
     [SerializeField] private Player player1;
     [SerializeField] private Player player2;
 
-    private CameraMovement[] cameras;
+    private List<CameraMovement> cameras;
     private Vector2 cameraInput1 => player1.InputDevice.GetCameraInput();
     private Vector2 cameraInput2 => player2.InputDevice.GetCameraInput();
     private Vector2 playerInput1 => player1.InputDevice.GetInput();
@@ -19,30 +20,35 @@ public class CamerasController : MonoBehaviour
     {
         Singleton = this;
 
-        cameras = new CameraMovement[2];
-        cameras[0] = player1.GetComponentInChildren<CameraMovement>();
-        cameras[1] = player2.GetComponentInChildren<CameraMovement>();
+        cameras = new List<CameraMovement>();
+        if (player1) cameras.Add(player1.GetComponentInChildren<CameraMovement>());
+        if (player2) cameras.Add(player2.GetComponentInChildren<CameraMovement>());
     }
 
     private void Update()
     {
         if (isSingleCamera)
         {
-            cameras[0].CameraInput = cameraInput1 + cameraInput2;
-            cameras[0].PlayerInput = playerInput1 + playerInput2;
-            cameras[1].CameraInput = cameraInput1 + cameraInput2;
-            cameras[1].PlayerInput = playerInput1 + playerInput2;
+            foreach (var cam in cameras)
+            {
+                cam.CameraInput = cameraInput1 + cameraInput2;
+                cam.PlayerInput = playerInput1 + playerInput2;
+            }
         }
         else
         {
             cameras[0].CameraInput = cameraInput1;
             cameras[0].PlayerInput = playerInput1;
-            cameras[1].CameraInput = cameraInput2;
-            cameras[1].PlayerInput = playerInput2;
+            if (cameras.Count > 1)
+            {
+                cameras[1].CameraInput = cameraInput2;
+                cameras[1].PlayerInput = playerInput2;
+            }
         }
 
         cameras[0].ContainerShift = isSingleCamera ? (player2.transform.position - player1.transform.position) / 2f : Vector3.zero;
-        cameras[1].ContainerShift = -cameras[0].ContainerShift;
+        if (cameras.Count > 1)
+            cameras[1].ContainerShift = -cameras[0].ContainerShift;
     }
 
     public void SetCamerasZoomOut(float currDist, float maxDist)
