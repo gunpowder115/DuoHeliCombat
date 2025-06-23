d1dfff3c03f0761a66c1e109486016ff7258d2ad
gunpowder115 <vk.poroh.alex@bk.ru>
2025-06-23 21:11:50 +0300

    Added parts for red man

--- HelicopterDemo/Assets/Scripts/Gameplay/SideShooter/SideShooter.cs
+++ HelicopterDemo/Assets/Scripts/Gameplay/SideShooter/SideShooter.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using static InputDeviceBase;
+using static Types;
 
 public class SideShooter : MonoBehaviour
 {
+    [SerializeField] private GlobalSide2 side = GlobalSide2.Blue;
+    [SerializeField] ControllerType controllerType = ControllerType.Keyboard;
 
+    private Shooter shooter;
+    private InputDeviceBase inputDevice;
+    private InputController inputController;
+
+    private void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+        if (shooter) shooter.Side = side;
+    }
+
+    private void Start()
+    {
+        inputController = InputController.singleton;
+        if (!inputController) return;
+        inputDevice = inputController.GetDevice(controllerType);
+    }
+
+    private void Update()
+    {
+        if (inputDevice.MinigunFire)
+            shooter.BarrelFire(null);
+        else
+            shooter.StopBarrelFire();
+    }
 }
